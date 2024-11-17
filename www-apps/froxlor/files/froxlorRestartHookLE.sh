e0559f2cdeb06c69e82936ba823786714a72fd47
z-e-r-0-t <35873310+z-e-r-0-t@users.noreply.github.com>
2024-11-17 15:37:19 +0100

    Make restart hook script compatible with systemd

--- www-apps/froxlor/files/froxlorRestartHookLE.sh
+++ www-apps/froxlor/files/froxlorRestartHookLE.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-rc-service -i -s postfix restart
-rc-service -i -s dovecot restart
-rc-service -i -s proftpd restart
-rc-service -i -s pure-ftpd restart
+if which rc-service &>/dev/null; then
+    rc-service -i -s postfix restart
+    rc-service -i -s dovecot restart
+    rc-service -i -s proftpd restart
+    rc-service -i -s pure-ftpd restart
+elif which systemctl &>/dev/null; then
+    systemctl is-active postfix && systemctl restart postfix
+    systemctl is-active dovecot && systemctl restart dovecot
+    systemctl is-active proftpd && systemctl restart proftpd
+    systemctl is-active pure-ftpd && systemctl restart pure-ftpd
+else
+    echo 'Error: RestartHookLE: missing rc-service and systemctl to restart services'
+    exit 1
+fi
